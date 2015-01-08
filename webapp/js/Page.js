c944f7fd8cedeb9fd622d3bd80cf7294faf3e709
meta-meta <paulmchristian@gmail.com>
2015-01-08 13:02:58 -0600

    wrong callback url

--- webapp/js/Page.js
+++ webapp/js/Page.js
@@ -22,7 +22,7 @@ define([
         componentDidMount: function () {
             SC.initialize({
                 client_id: '816559a8805e07de7b585e3e588d5e7e',
-                redirect_uri: 'http://meta-meta.github.io/SoundCloud/callback.html'
+                redirect_uri: 'http://meta-meta.github.io/SoundCloud/webapp/callback.html'
                 //redirect_uri: 'http://localhost:8000/callback.html'
             });
 
